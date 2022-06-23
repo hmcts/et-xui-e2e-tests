5fbaa3010d18f7eff18ebf1409aa891bee9220e3
Sunday Ayeni <sunnyayeni18@gmail.com>
2022-06-23 16:53:12 +0100

    update test files

--- test/util.js
+++ test/util.js
@@ -1,13 +1,13 @@
 const { I } = inject();
 
-let username = process.env.TEST_CASE_USERNAME || 'et.caseworker.3@hmcts.net';
-let password = process.env.TEST_CASE_PASSWORD || '6G!qmCd3';
+let username = process.env.TEST_CASE_USERNAME || '';
+let password = process.env.TEST_CASE_PASSWORD || '';
 
-function signInWithCredentials() {
+async function signInWithCredentials() {
   I.waitForElement('#username', 30);
   console.log(username, password);
   I.fillField('#username', username);
   I.fillField('#password', password);
-  I.click('Sign in');
+  await I.click('Sign in');
 }
 module.exports = { signInWithCredentials };
