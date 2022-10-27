b09beb5cb359c715c7dfa6abfc9a5fa200242b46
Pettedson John <pettedson.john@hmcts.net>
2022-10-27 21:13:40 +0100

    Added all of the ET1 Acceptance and Serving Tests....

--- test/e2e/scenario/ETCaseSubmission-ET1Verification.js
+++ test/e2e/scenario/ETCaseSubmission-ET1Verification.js
@@ -5,4 +5,21 @@ Scenario('Make Draft Application', async ({ I, basePage, loginPage }) => {
   I.amOnPage('/');
   await basePage.processPreLoginPagesForTheDraftApplication();
   await loginPage.processLogin(testConfig.TestEnvETUser, testConfig.TestEnvETPassword);
-}).tag('@RET-BAT');
+}).tag('@RET-WIP');
+
+Scenario(
+  'Login the Manage Case and Submit and ET1 Vetting',
+  async ({ I, loginPage, caseListPage, et1CaseVettingPages, et1CaseServingPages }) => {
+    I.amOnPage(testConfig.TestUrlForManageCaseAAT);
+    await loginPage.processLogin(testConfig.TestEnvETManageCaseUserAAT, testConfig.TestEnvETManageCasePasswordAAT);
+    await caseListPage.searchCaseApplicationWithSubmissionReference('2: Object', '1666901323048647');
+    let caseNumber = await caseListPage.processCaseFromCaseList();
+    console.log('The value of the Case Number ' + caseNumber);
+    //await citizenHubPages.verifyCitizenHubCaseOverviewPage(caseNumber,'1666891874114742'); Test after the Citizen Hub Login is already in Session....
+    await caseListPage.verifyCaseDetailsPage();
+    await caseListPage.selectNextEvent('1: Object'); //Firing the ET1 Event.
+    await et1CaseVettingPages.processET1CaseVettingPages(caseNumber);
+    //await caseListPage.verifyCaseDetailsPage(true);
+    await et1CaseServingPages.processET1CaseServingPages(caseNumber);
+  },
+).tag('@RET-BAT');
