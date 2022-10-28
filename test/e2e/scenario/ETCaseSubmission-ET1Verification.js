2df139c88e44e7d907217bd140ae693dd1752401
Pettedson John <pettedson.john@hmcts.net>
2022-10-28 11:29:25 +0100

    Fixes for Integration and Complete Testing....

--- test/e2e/scenario/ETCaseSubmission-ET1Verification.js
+++ test/e2e/scenario/ETCaseSubmission-ET1Verification.js
@@ -12,17 +12,12 @@ Scenario(
     await employmentAndRespondentDetailsPage.processStillWorkingJourney();
   },
 ).tag('@RET-BAT');
-Scenario('Make Draft Application', async ({ I, basePage, loginPage }) => {
-  I.amOnPage('/');
-  await basePage.processPreLoginPagesForTheDraftApplication();
-  await loginPage.processLogin(testConfig.TestEnvETUser, testConfig.TestEnvETPassword);
-}).tag('@RET-WIP');
 
 Scenario(
   'Login the Manage Case and Submit and ET1 Vetting',
   async ({ I, loginPage, caseListPage, et1CaseVettingPages, et1CaseServingPages }) => {
     I.amOnPage(testConfig.TestUrlForManageCaseAAT);
-    await loginPage.processLogin(testConfig.TestEnvETManageCaseUserAAT, testConfig.TestEnvETManageCasePasswordAAT);
+    await loginPage.processLogin(testConfig.TestEnvETManageCaseUser, testConfig.TestEnvETManageCasePassword);
     await caseListPage.searchCaseApplicationWithSubmissionReference('2: Object', '1666901323048647');
     let caseNumber = await caseListPage.processCaseFromCaseList();
     console.log('The value of the Case Number ' + caseNumber);
@@ -33,4 +28,4 @@ Scenario(
     //await caseListPage.verifyCaseDetailsPage(true);
     await et1CaseServingPages.processET1CaseServingPages(caseNumber);
   },
-).tag('@RET-BAT');
+).tag('@RET-WIP');
