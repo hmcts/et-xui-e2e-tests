34880b488c99bbafa45844d042cf4850a9ae5ed6
Pettedson John <pettedson.john@hmcts.net>
2022-10-28 11:29:25 +0100

    Fixes for Integration and Complete Testing....

--- test/e2e/config.js
+++ test/e2e/config.js
@@ -1,13 +1,15 @@
 module.exports = {
   TestUrl: process.env.TEST_E2E_URL || 'https://et-sya.aat.platform.hmcts.net',
-  TestUrlForManageCaseAAT: 'https://manage-case.aat.platform.hmcts.net',
-  TestShowBrowserWindow: process.env.SHOW_BROWSER_WINDOW || false,
+  TestUrlForManageCaseAAT: process.env.TEST_MANAGE_CASE_URL || 'https://manage-case.aat.platform.hmcts.net',
+  TestShowBrowserWindow: process.env.SHOW_BROWSER_WINDOW || true,
   TestsPathToRun: process.env.E2E_TEST_PATH || './**/*.js',
   TestReportFolder: process.env.E2E_OUTPUT_DIR || './functional-output/reports',
+
   TestEnvETUser: process.env.TEST_CASE_USERNAME || '',
   TestEnvETPassword: process.env.TEST_CASE_PASSWORD || '',
-  TestEnvETManageCaseUserAAT: process.env.TEST_MANAGE_CASE_USERNAME_AAT || '',
-  TestEnvETManageCasePasswordAAT: process.env.TEST_MANAGE_CASE_PASSWORD_AAT || '',
+  TestEnvETManageCaseUser: process.env.TEST_MANAGE_CASE_USERNAME || '',
+  TestEnvETManageCasePassword: process.env.TEST_MANAGE_CASE_PASSWORD || '',
+
   TestRunningEnvironment: process.env.RUNNING_ENV || 'aat',
   TestIDAMEnvironment: process.env.IDAM_ENV || 'aat',
   TestPrNumber: process.env.PR_NUMBER || 'pr-972',
