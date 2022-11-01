ac0729715fc06d5893839e8b22cf1e5f4efac681
Pettedson John <pettedson.john@hmcts.net>
2022-11-01 15:35:42 +0000

    Fixed the grabTextFrom trim() function call...

--- test/pages/submitClaim.page.js
+++ test/pages/submitClaim.page.js
@@ -1,11 +1,11 @@
 const { I } = inject();
 
 module.exports = {
-  submitClaim() {
+  async submitClaim() {
     this.clickCheckYourAnswersLink();
     this.noPcqQuestions();
     this.clickSubmitOnCheckYourAnswers();
-    return this.verifyClaimSubmitted();
+    return await this.verifyClaimSubmitted();
   },
   //user clicks check your answers link
   clickCheckYourAnswersLink() {
@@ -21,9 +21,9 @@ module.exports = {
     I.waitForText('Check your answers', 30);
     I.click('Submit');
   },
-  verifyClaimSubmitted() {
+  async verifyClaimSubmitted() {
     I.waitForText('Your claim has been submitted', 30);
-    const submissionRef = I.grabTextFrom('//*[@id="main-content"]/div[1]/div/dl[1]/div[1]/dd').trim();
+    const submissionRef = (await I.grabTextFrom('//*[@id="main-content"]/div[1]/div/dl[1]/div[1]/dd')).trim();
     console.log(submissionRef);
     return submissionRef;
   },
