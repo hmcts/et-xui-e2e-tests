276e4bdeb7a1ec1a52cd47c7ffd15cba9df64a57
Pettedson John <pettedson.john@hmcts.net>
2022-11-01 13:51:42 +0000

    Adding the changes after by removing await and async's from the base page....

--- test/pages/caselist.page.js
+++ test/pages/caselist.page.js
@@ -19,14 +19,14 @@ module.exports = {
   nextEventDropdown: '#next-step',
   submitEventButton: '[type="submit"]',
 
-  async searchCaseApplication(option) {
+  searchCaseApplication(option) {
     I.see(this.caseListText);
     I.waitForElement(this.caseTypeDropdown, 30);
     I.selectOption(this.caseTypeDropdown, option);
-    await I.click(this.applyButton);
+    I.click(this.applyButton);
   },
 
-  async searchCaseApplicationWithSubmissionReference(option, submissionReference) {
+  searchCaseApplicationWithSubmissionReference(option, submissionReference) {
     I.waitForInvisible('.spinner-container', 30);
     I.waitForElement(this.caseTypeDropdown, 30);
     I.wait(7);
@@ -34,10 +34,10 @@ module.exports = {
     I.waitForElement(this.submissionReferenceLocator, 30);
     I.see(this.caseListText);
     I.fillField(this.submissionReferenceLocator, submissionReference);
-    await I.click(this.applyButton);
+    I.click(this.applyButton);
   },
 
-  async processCaseFromCaseList() {
+  processCaseFromCaseList() {
     I.waitForText('Your cases', 30);
     let caseNumber = I.grabTextFrom(
       '//html[1]/body[1]/exui-root[1]/exui-case-home[1]/div[1]/exui-case-list[1]/exui-page-wrapper[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[2]/ccd-search-result[1]/table[1]/tbody[1]/tr[1]/td[1]/a[1]/ccd-field-read[1]/div[1]/ccd-field-read-label[1]/div[1]/ng-component[1]/span[1]',
@@ -48,15 +48,15 @@ module.exports = {
     return caseNumber;
   },
 
-  async selectNextEvent(option) {
+  selectNextEvent(option) {
     //I.waitForElement(this.nextEventDropdown, 60);
     I.waitForEnabled(this.nextEventDropdown, 60);
     //await I.click(this.nextEventDropdown);
-    await I.selectOption(this.nextEventDropdown, option);
-    await I.click(this.submitEventButton);
+    I.selectOption(this.nextEventDropdown, option);
+    I.click(this.submitEventButton);
   },
 
-  async verifyCaseDetailsPage(et1VettingFlag = false) {
+  verifyCaseDetailsPage(et1VettingFlag = false) {
     I.waitForText('Case Details', 30);
     I.see('Claimant');
     I.see('Respondent');
