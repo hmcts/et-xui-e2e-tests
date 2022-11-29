257a0f80da3d82e3a891c36d6b43aae46b949463
Sunday Ayeni <sunnyayeni18@gmail.com>
2022-11-29 13:35:51 +0000

    initial test

--- test/pages/citizenhub.pages.js
+++ test/pages/citizenhub.pages.js
@@ -18,4 +18,20 @@ module.exports = {
     I.see('We aim to process your claim by');
     I.see('In busy periods it may take longer.');
   },
+
+  verifyListOfTribunalOffices() {
+    I.click('//a[contains(.,"Contact the tribunal about my case")]');
+    I.see('Contact the tribunal about your case');
+    I.click('.govuk-accordion__show-all-text');
+    I.see('Give notice that I want to withdraw all or part of my claim');
+    I.see('I want to change my personal details');
+    I.see('Apply to postpone my hearing');
+    I.see('Apply to vary or revoke an order');
+    I.see('Apply to have a decision considered afresh');
+    I.see('Apply to amend my claim');
+    I.see('Order the respondent to do something');
+    I.see('Order a witness to attend to give evidence');
+    I.see('Tell the tribunal the respondent has not complied with an order');
+    //To do
+  },
 };
