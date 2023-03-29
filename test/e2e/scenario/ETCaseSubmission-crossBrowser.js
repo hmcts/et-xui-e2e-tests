c0311c7b4b00f6d2d31c8c010170a1c5848d9ea2
Sunday Ayeni <sunnyayeni18@gmail.com>
2023-03-29 14:20:14 +0100

    fix cross browser

--- test/e2e/scenario/ETCaseSubmission-crossBrowser.js
+++ test/e2e/scenario/ETCaseSubmission-crossBrowser.js
@@ -1,39 +1,46 @@
 const testConfig = require('../config.js');
+const postcode = 'LS9 9HE';
+const workPostcode = 'LS7 4QE';
+const selectedWorkAddress =
+  '{"fullAddress":"7, VALLEY GARDENS, LEEDS, LS7 4QE","street1":"7, VALLEY GARDENS","street2":"","town":"LEEDS","county":"LEEDS","postcode":"LS7 4QE","country":"ENGLAND"}';
+const addressOption =
+  '{"fullAddress":"3, SKELTON AVENUE, LEEDS, LS9 9HE","street1":"3, SKELTON AVENUE","street2":"","town":"LEEDS","county":"LEEDS","postcode":"LS9 9HE","country":"ENGLAND"}';
+const firstLineOfAddress = '7, VALLEY GARDENS?';
 
 Feature('End To End Tests For an ET Case Submitted in the sya Front end and processed in the Manage Case Application');
 Scenario(
   'Create a claim for still working for organisation, submit and process within manage cases : Crossbrowser Tests',
   async ({
-    I,
-    basePage,
-    loginPage,
-    taskListPage,
-    personalDetailsPage,
-    employmentAndRespondentDetailsPage,
-    claimDetailsPage,
-    submitClaimPage,
-    caseListPage,
-    et1CaseVettingPages,
-    et1CaseServingPages,
-  }) => {
+           I,
+           basePage,
+           loginPage,
+           taskListPage,
+           personalDetailsPage,
+           employmentAndRespondentDetailsPage,
+           claimDetailsPage,
+           submitClaimPage,
+           caseListPage,
+           et1CaseVettingPages,
+           et1CaseServingPages,
+           // citizenHubPages,
+         }) => {
     I.amOnPage('/');
-    await basePage.processPreLoginPagesForTheDraftApplication(); //Submission in the sya front end.
+    await basePage.processPreLoginPagesForTheDraftApplication(postcode);
     await loginPage.processLogin(testConfig.TestEnvETUser, testConfig.TestEnvETPassword);
     await taskListPage.processPostLoginPagesForTheDraftApplication();
-    await personalDetailsPage.processPersonalDetails();
-    I.wait(5);
-    await employmentAndRespondentDetailsPage.processStillWorkingJourney();
-    I.wait(5);
+    await personalDetailsPage.processPersonalDetails(postcode, 'England', addressOption);
+    await employmentAndRespondentDetailsPage.processStillWorkingJourney(
+      workPostcode,
+      selectedWorkAddress,
+      firstLineOfAddress,
+    );
     await claimDetailsPage.processClaimDetails();
-    I.wait(5);
-    const submissionReference = await submitClaimPage.submitClaim();
+    let submissionReference = await submitClaimPage.submitClaim();
     I.click('Sign out');
-
     I.amOnPage(testConfig.TestUrlForManageCaseAAT);
-    await loginPage.processLogin(testConfig.TestEnvETManageCaseUser, testConfig.TestEnvETManageCasePassword); //Manage Case Application
-    await caseListPage.searchCaseApplicationWithSubmissionReference('3: Object', submissionReference);
-    I.wait(5);
-    let caseNumber = await caseListPage.processCaseFromCaseList();
+    await loginPage.processLogin(testConfig.TestEnvETManageCaseUser, testConfig.TestEnvETManageCasePassword);
+    await caseListPage.searchCaseApplicationWithSubmissionReference('5: Object', submissionReference);
+    let caseNumber = await caseListPage.processCaseFromCaseList(submissionReference);
     console.log('The value of the Case Number ' + caseNumber);
     //await citizenHubPages.verifyCitizenHubCaseOverviewPage(caseNumber,'1666891874114742'); Test after the Citizen Hub Login is already in Session....
     await caseListPage.verifyCaseDetailsPage();
@@ -42,8 +49,14 @@ Scenario(
     //await caseListPage.verifyCaseDetailsPage(true);
     await et1CaseServingPages.processET1CaseServingPages(caseNumber);
     I.click('Sign out');
-    //To be Enabled and Retested after Defect RET-2735 is fixed.
-    //await citizenHubPages.processLogin(testConfig.TestEnvETUser, testConfig.TestEnvETPassword, submissionReference);
-    //await citizenHubPages.verifyCitizenHubCaseOverviewPage(caseNumber);
+    /*    await citizenHubPages.processCitizenHubLogin(
+      testConfig.TestEnvETUser,
+      testConfig.TestEnvETPassword,
+      submissionReference,
+    );
+    await citizenHubPages.clicksViewLinkOnClaimantApplicationPage(caseNumber, submissionReference);
+    await citizenHubPages.verifyCitizenHubCaseOverviewPage(caseNumber);
+    await citizenHubPages.VerifyFormType();
+    */
   },
 ).tag('@RET-XB');
