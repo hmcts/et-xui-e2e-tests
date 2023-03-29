be31395f08f4b6cdfc5f1b4ada47bb05e7fcae6b
Sunday Ayeni <sunnyayeni18@gmail.com>
2023-03-29 19:35:54 +0100

    fix crossbrowser scenario part 3

--- test/pages/et1caseserving.pages.js
+++ test/pages/et1caseserving.pages.js
@@ -21,14 +21,16 @@ module.exports = {
     I.see('Case Number: ' + caseNumber);
     I.checkOption(this.locators.pre_accept_case_yes_option);
     I.fillField(this.locators.date_accepted_day, '27');
-    I.fillField(this.locators.date_accepted_month, '03');
+    I.fillField(this.locators.date_accepted_month, '06');
     I.fillField(this.locators.date_accepted_year, '2023');
     I.click('Continue');
+
   },
 
   processAcceptRejectCase(caseNumber) {
+    I.waitForVisible("[type='submit']", 10);
     I.waitForText('Case Number: ' + caseNumber, 30);
-    I.waitForElement("[type='submit']", 30);
     I.forceClick("[type='submit']");
+    I.wait(5)
   },
 };
