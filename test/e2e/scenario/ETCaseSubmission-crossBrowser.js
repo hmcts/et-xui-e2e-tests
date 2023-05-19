9292efd077410989a248d12810a3a824c3b9a577
Sunday Ayeni <sunnyayeni18@gmail.com>
2023-05-19 16:27:53 +0100

    update xbrowser test

--- test/e2e/scenario/ETCaseSubmission-crossBrowser.js
+++ test/e2e/scenario/ETCaseSubmission-crossBrowser.js
@@ -1,11 +1,10 @@
 const testConfig = require('../config.js');
 const postcode = 'LS9 9HE';
 const workPostcode = 'LS7 4QE';
-const selectedWorkAddress =
-  '{"fullAddress":"7, VALLEY GARDENS, LEEDS, LS7 4QE","street1":"7, VALLEY GARDENS","street2":"","town":"LEEDS","county":"LEEDS","postcode":"LS7 4QE","country":"ENGLAND"}';
+const selectedWorkAddress = '7, Valley Gardens, Leeds, LS7 4QE'
 const addressOption =
-  '{"fullAddress":"3, SKELTON AVENUE, LEEDS, LS9 9HE","street1":"3, SKELTON AVENUE","street2":"","town":"LEEDS","county":"LEEDS","postcode":"LS9 9HE","country":"ENGLAND"}';
-const firstLineOfAddress = '7, VALLEY GARDENS?';
+  '3, Skelton Avenue, Leeds, LS9 9HE';
+const firstLineOfAddress = '7, Valley Gardens?';
 
 Feature('End To End Tests For an ET Case Submitted in the sya Front end and processed in the Manage Case Application');
 Scenario(
@@ -29,11 +28,9 @@ Scenario(
     await loginPage.processLogin(testConfig.TestEnvETUser, testConfig.TestEnvETPassword);
     await taskListPage.processPostLoginPagesForTheDraftApplication();
     await personalDetailsPage.processPersonalDetails(postcode, 'England', addressOption);
-    await employmentAndRespondentDetailsPage.processStillWorkingJourney(
-      workPostcode,
+    await employmentAndRespondentDetailsPage.processWorkingNoticePeriodJourney(workPostcode,
       selectedWorkAddress,
-      firstLineOfAddress,
-    );
+      firstLineOfAddress);
     await claimDetailsPage.processClaimDetails();
     let submissionReference = await submitClaimPage.submitClaim();
     I.click('Sign out');
@@ -49,7 +46,7 @@ Scenario(
     //await caseListPage.verifyCaseDetailsPage(true);
     await caseListPage.selectNextEvent('2: Object'); //Case acceptance or rejection Event
     await et1CaseServingPages.processET1CaseServingPages(caseNumber);
-    I.forceClick('Sign out');;
+    I.click('Sign out');
     /*    await citizenHubPages.processCitizenHubLogin(
       testConfig.TestEnvETUser,
       testConfig.TestEnvETPassword,
