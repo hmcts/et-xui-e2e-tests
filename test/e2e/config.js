229b6e6f3d8dd5c35b5f794d671222f6248b1639
SunnyAyeni <95691644+SunnyAyeni@users.noreply.github.com>
2023-05-23 14:06:51 +0100

    Update config.js

--- test/e2e/config.js
+++ test/e2e/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  TestUrl: process.env.TEST_URL || 'https://et-sya.demo.platform.hmcts.net',
-  TestUrlForManageCaseAAT: process.env.TEST_MANAGE_CASE_URL || 'https://manage-case.demo.platform.hmcts.net',
+  TestUrl: process.env.TEST_URL || '',
+  TestUrlForManageCaseAAT: process.env.TEST_MANAGE_CASE_URL || '',
   TestShowBrowserWindow: process.env.SHOW_BROWSER_WINDOW || false,
   TestsPathToRun: process.env.E2E_TEST_PATH || './**/*.js',
   TestReportFolder: process.env.E2E_OUTPUT_DIR || './functional-output/reports',
-  TestEnvETUser: process.env.TEST_CASE_USERNAME || 'et.citizen1@hmcts.net',
-  TestEnvETPassword: process.env.TEST_CASE_PASSWORD || 'ZQ78vl&1',
-  TestEnvETManageCaseUser: process.env.ET_CASEWORKER_USER_NAME || 'et.caseworker.4@hmcts.net',
-  TestEnvETManageCasePassword: process.env.ET_CASEWORKER_PASSWORD || 'eHd7yB%u',
+  TestEnvETUser: process.env.TEST_CASE_USERNAME || '',
+  TestEnvETPassword: process.env.TEST_CASE_PASSWORD || '',
+  TestEnvETManageCaseUser: process.env.ET_CASEWORKER_USER_NAME || '',
+  TestEnvETManageCasePassword: process.env.ET_CASEWORKER_PASSWORD || '',
 };
