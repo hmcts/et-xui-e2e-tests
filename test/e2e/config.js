828b416f4599bfc08ded6b4007f3b1b78c90d4d8
Sunday Ayeni <sunnyayeni18@gmail.com>
2023-05-23 16:14:19 +0100

    deal with cookie

--- test/e2e/config.js
+++ test/e2e/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   TestUrl: process.env.TEST_URL || 'https://et-sya.aat.platform.hmcts.net',
   TestUrlForManageCaseAAT: process.env.TEST_MANAGE_CASE_URL || 'https://manage-case.aat.platform.hmcts.net',
-  TestShowBrowserWindow: process.env.SHOW_BROWSER_WINDOW || true,
+  TestShowBrowserWindow: process.env.SHOW_BROWSER_WINDOW || false,
   TestsPathToRun: process.env.E2E_TEST_PATH || './**/*.js',
   TestReportFolder: process.env.E2E_OUTPUT_DIR || './functional-output/reports',
   TestEnvETUser: process.env.TEST_CASE_USERNAME || '',
