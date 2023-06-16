37bc6503728d8b896d0e46dcba3b2d16c911e90d
nish <mnbdba@gmail.com>
2023-06-16 14:10:13 +0100

    Respond - Scotland

--- test/e2e/scenario/ETCaseSubmission-MakeAnApplication.js
+++ test/e2e/scenario/ETCaseSubmission-MakeAnApplication.js
@@ -83,3 +83,81 @@ Scenario(
     */
   },
 ).tag('@RET-3687');
+
+Scenario(
+  'Create a claim for still working for organisation, submit and Respond to an application',
+  async ({
+    I,
+    basePage,
+    loginPage,
+    taskListPage,
+    personalDetailsPage,
+    employmentAndRespondentDetailsPage,
+    claimDetailsPage,
+    submitClaimPage,
+    caseListPage,
+    makeanApplicationPage,
+    et1CaseVettingPages,
+    et1CaseServingPages,
+    // citizenHubPages,
+  }) => {
+    I.amOnPage('/');
+    // await basePage.processPreLoginPagesForTheDraftApplication(postcode);
+    // await loginPage.processLogin(testConfig.TestEnvETUser, testConfig.TestEnvETPassword);
+    // await taskListPage.processPostLoginPagesForTheDraftApplication();
+    // await personalDetailsPage.processPersonalDetails(postcode, 'England', addressOption);
+    // await employmentAndRespondentDetailsPage.processStillWorkingJourney(
+    //   workPostcode,
+    //   selectedWorkAddress,
+    //   firstLineOfAddress,
+    // );
+    // await claimDetailsPage.processClaimDetails();
+    // let submissionReference = await submitClaimPage.submitClaim();
+    // I.click('Sign out');
+    let submissionReference = '1683876566560350';
+    I.amOnPage(testConfig.TestUrlForManageCaseAAT);
+    // await loginPage.processLogin(testConfig.TestEnvETLegalRepUser, testConfig.TestEnvETLegalRepPassword);
+    // await caseListPage.searchCaseApplicationWithSubmissionReference('Eng/Wales - Singles', submissionReference);
+    // let caseNumber = await caseListPage.processCaseFromCaseList(submissionReference);
+    // console.log('The value of the Case Number ' + caseNumber);
+    // await caseListPage.selectTab('Applications');
+    // await caseListPage.navigateToMakeAnApplication(submissionReference);
+    // await makeanApplicationPage.selectApplicationType('Amend response');
+    // await makeanApplicationPage.amendResponse('Amend response');
+    // await makeanApplicationPage.copyCorrespondance();
+    // await makeanApplicationPage.checkYourAnswersAndSybmit();
+    // await makeanApplicationPage.closeAndReturnToCaseDetails();
+    // I.click('Sign out');
+    await loginPage.processLogin(testConfig.TestEnvETManageCaseUser, testConfig.TestEnvETManageCasePassword);
+    await caseListPage.searchCaseApplicationWithSubmissionReference('Scotland - Singles (RET)', submissionReference);
+    await caseListPage.processCaseFromCaseList(submissionReference);
+    await caseListPage.selectTab('Applications');
+    await makeanApplicationPage.respondToAnApplication(submissionReference);
+    await makeanApplicationPage.selectApplicationType('Postpone a hearing');
+    await makeanApplicationPage.selectcaseManagementOption('caseManagementOrder');
+    await makeanApplicationPage.selectPartyType('bothParties');
+    await makeanApplicationPage.selectcaseManagementOrder('Legal officer');
+    await makeanApplicationPage.selectResponseTotribunal('No');
+    await makeanApplicationPage.submitApplication();
+    await makeanApplicationPage.checkYourAnswersAndSybmit();
+    await makeanApplicationPage.closeAndReturnToCaseDetails();
+
+    //await citizenHubPages.verifyCitizenHubCaseOverviewPage(caseNumber,'1666891874114742'); Test after the Citizen Hub Login is already in Session....
+    //await caseListPage.verifyCaseDetailsPage();
+    //await caseListPage.selectNextEvent('1: Object'); //Firing the ET1 Event.
+    //await et1CaseVettingPages.processET1CaseVettingPages(caseNumber);
+    //await caseListPage.verifyCaseDetailsPage(true);
+    //await caseListPage.selectNextEvent('2: Object'); //Case acceptance or rejection Event
+    //await et1CaseServingPages.processET1CaseServingPages(caseNumber);
+    //I.click('Sign out');
+    /*    await citizenHubPages.processCitizenHubLogin(
+      testConfig.TestEnvETUser,
+      testConfig.TestEnvETPassword,
+      submissionReference,
+    );
+    await citizenHubPages.clicksViewLinkOnClaimantApplicationPage(caseNumber, submissionReference);
+    await citizenHubPages.verifyCitizenHubCaseOverviewPage(caseNumber);
+    await citizenHubPages.VerifyFormType();
+    */
+  },
+).tag('@RET-3687-Scotland');
