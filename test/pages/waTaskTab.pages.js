d6feeb58f5191fbd30d1d44848da941898502d78
Sunday Ayeni <sunnyayeni18@gmail.com>
2023-11-17 10:21:13 +0000

    fix lint issue

--- test/pages/waTaskTab.pages.js
+++ test/pages/waTaskTab.pages.js
@@ -1,11 +1,10 @@
 const { I } = inject();
-const testConfig = require('../../config');
 
 module.exports = {
 
   et1VettingLink: '//a[contains(.,"ET1 Vetting")]',
 
-  verifyWAtaskTabPage(submissionReference) {
+  verifyWAtaskTabPage() {
     I.waitForElement(this.et1VettingLink, 20);
     I.click(this.et1VettingLink);
   },
