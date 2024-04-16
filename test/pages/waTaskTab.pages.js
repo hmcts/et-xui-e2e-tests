ee62ea6591aafe74c50b9fa488d854ff72a0ae33
nishm <mnbdba@gmail.com>
2024-04-16 20:48:30 +0100

    Issue Draft and SIgn Judgement as a Admin and Issue Judgement as a Judge (#346)
    * Fixed conflicts
    
    * Added Issue Judgement for eng/wales
    
    * PR comments
    
    * updated config

--- test/pages/waTaskTab.pages.js
+++ test/pages/waTaskTab.pages.js
@@ -2,14 +2,69 @@ const { I } = inject();
 
 module.exports = {
   assignToMe: '#action_claim',
+  assignTo: '#action_assign',
   myWorkTabLink: '//a[contains(.,"My work")]',
   manageTaskLink: '.govuk-accordion__section-button',
+  judicialOption: '#JUDICIAL',
+  legalOption: '#LEGAL_OPERATIONS',
   goToTaskLink: '#action_go',
+  selectPersonInput: '#inputSelectPerson',
+  selectPerson: '#mat-option-0 > span',
+  isJudgementYes: '#draftAndSignJudgement_isJudgement_Yes',
+  uploadFileButton: '[type="file"]',
+
   async clickAssignToMeLink(task) {
     let taskLocator = `//a[contains(.,"${task}")]`;
     await this.verifyTaskIsAvailable();
     I.waitForElement(taskLocator, 25);
     await I.click(taskLocator);
+  },
+
+  async clickAssignToLink(task, roleType) {
+    I.scrollPageToBottom();
+
+    let taskContainer = `//exui-case-task[.//strong[contains(.,"${task}")]]`;
+    let assignTaskAnchor = '#event > div > div > div > exui-case-task:nth-child(6)  #action_assign';
+
+    await I.wait(90);
+    await I.refreshPage();
+    await I.wait(10);
+    await I.scrollPageToBottom();
+    await I.wait(10);
+    await I.scrollTo(taskContainer);
+    await I.click(assignTaskAnchor);
+    await I.wait(10);
+    if (roleType === 'JUDICIAL') {
+      await I.checkOption(this.judicialOption);
+      await I.click('Continue');
+    } else {
+      await I.checkOption(this.legalOption);
+      await I.click('Continue');
+    }
+  },
+
+  async assignToAPerson(person) {
+    I.waitForElement(this.selectPersonInput, 15);
+    I.fillField(this.selectPersonInput, person);
+    I.waitForElement(this.selectPerson, 10);
+    I.click(this.selectPerson);
+    I.wait(5);
+    I.click('Continue');
+    I.waitForElement('#main-content button', 10);
+    I.click('#main-content button');
+  },
+
+  async issueJudgeMent(task) {
+    let taskLocator = `//a[contains(.,"${task}")]`;
+    I.waitForElement(taskLocator, 25);
+    await I.click(taskLocator);
+    await I.see('Draft and Sign Judgement');
+    await I.waitForElement(this.isJudgementYes, 10);
+    I.attachFile(this.uploadFileButton, '/test/data/RET_newBug.png');
+    I.click('Continue');
+    I.waitForText('Check your answers', 5);
+    I.click('Submit');
+    I.waitForText('has been updated with event: Draft and Sign Judgement', 10);
   },
 
   async isTaskVisible(taskLocator) {
