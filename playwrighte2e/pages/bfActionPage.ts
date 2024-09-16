de1a0c68744c2ab56d273bf3c813232b0cb69f94
Nilamkanojiya <nilam_kanojiya@yahoo.com>
2024-09-16 14:50:36 +0100

    add new test

--- playwrighte2e/pages/bfActionPage.ts
+++ playwrighte2e/pages/bfActionPage.ts
@@ -13,7 +13,7 @@ export default class BfActionPage extends BasePage {
   async addBfAction() {
     await expect(this.page.locator('ccd-case-edit-page')).toContainText('B/F Action');
     await this.page.getByRole('button', { name: 'Add new' }).click();
-    await this.page.locator('#bfActions_0_cwActions').selectOption('Application of letter to ACAS/RPO');
+    await this.page.locator(this.elements.bfActionDropDown).selectOption('Application of letter to ACAS/RPO');
     await this.page.locator(this.elements.bfDate).fill('11');
     await this.page.locator(this.elements.bfMonth).fill('09');
     await this.page.locator(this.elements.bfYear).fill('2024');
