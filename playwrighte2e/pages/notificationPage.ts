ab9550f85cb6113a55fb4d28eb9bd9cb7b45d8b0
Nilamkanojiya <nilam_kanojiya@yahoo.com>
2024-10-07 17:10:09 +0100

    add notification test

--- playwrighte2e/pages/notificationPage.ts
+++ playwrighte2e/pages/notificationPage.ts
@@ -1,12 +1,11 @@
 import { BasePage } from "./basePage";
 import { expect } from "@playwright/test";
-import { th } from "@faker-js/faker";
 
 
 export default class NotificationPage extends BasePage {
 
   elements = {
-
+notificationDropDown:'#claimantSelectNotification'
   }
 
   async sendNotification(){
@@ -20,6 +19,17 @@ export default class NotificationPage extends BasePage {
     await this.page.getByText('Claim (ET1)').click();
     await this.submitButton();
     await this.page.getByRole('button', { name: 'Close and Return to case' }).click();
+  }
+
+  async viewNotification(){
+    await this.page.getByText('Judgments, orders &').click();
+    await this.page.getByRole('link', { name: 'View a judgment, order or' }).click();
+    await expect(this.page.locator('ccd-case-edit-page')).toContainText('View a judgment, order or notification');
+    await this.page.locator(this.elements.notificationDropDown).selectOption('1: 1');
+    await this.page.getByRole('button', { name: 'Continue' }).click();
+    await expect(this.page.locator('tbody')).toContainText('test Notification');
+    await expect(this.page.locator('thead')).toContainText('View Notification');
+    await expect(this.page.locator('tbody')).toContainText('Both parties');
   }
 
 }
