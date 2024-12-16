98ec0865ae1d63dd220d9deabcde9157f195f8db
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2024-12-16 13:26:08 +0000

    Updates
    Signed-off-by: Harpreet Jhita <harpreet.jhita@justice.gov.uk>

--- playwrighte2e/pages/nocPage.ts
+++ playwrighte2e/pages/nocPage.ts
@@ -1,6 +1,5 @@
 import { BasePage } from "./basePage";
 import { expect } from "@playwright/test";
-import { th } from "@faker-js/faker";
 
 export default class NocPage extends BasePage{
   elements={
@@ -18,7 +17,6 @@ export default class NocPage extends BasePage{
     await expect(this.page.locator('label')).toContainText('Online case reference number');
     await this.page.locator(this.elements.caseRef).fill(submissionRef);
     await this.clickContinue();
-    await expect(this.page.locator('h1')).toContainText('Enter details');
     await this.page.locator(this.elements.respName).fill('Mark McDonald');
     await this.page.locator(this.elements.claimantFirstName).fill('Jessamine');
     await this.page.locator(this.elements.claimantLastName).fill('Malcom');
