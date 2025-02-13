0c14add5895c113ae8a693b05f5424ffacf21832
Nilamkanojiya <nilam_kanojiya@yahoo.com>
2025-02-13 17:20:38 +0000

    make an pplication 13/2

--- playwrighte2e/tests/LegalRepMakeApplication.test.ts
+++ playwrighte2e/tests/LegalRepMakeApplication.test.ts
@@ -5,7 +5,7 @@ const respondentName = 'Mrs Test Auto';
 let subRef: string, submissionRef: string;
 let caseNumber;
 
-test.describe('Make an application', () => {
+test.describe('Make an application and view Recorded Decision', () => {
 
     test.beforeEach(async ({ page,createCaseStep }) => {
 
@@ -13,7 +13,7 @@ test.describe('Make an application', () => {
        // subRef = '1739442740238944';
     });
 
-    test.skip('Legal representatives make and application - England', async ({ page,citizenHubPage,loginPage,legalRepPage,et1CaseServingPage,caseListPage, applicationTabPage  }) => {
+    test('Legal representatives make and application - England', async ({ page,citizenHubPage,loginPage,legalRepPage,et1CaseServingPage,caseListPage, applicationTabPage  }) => {
         const { firstName, lastName } = await et1CaseServingPage.getClaimantFirstName();
         //perform NOC
         await page.click('text=Sign out');
@@ -37,10 +37,36 @@ test.describe('Make an application', () => {
         await caseListPage.searchCaseApplicationWithSubmissionReference('Eng/Wales - Singles', subRef);
         caseNumber = await caseListPage.processCaseFromCaseList();
         await applicationTabPage.respondToAnApplication();
+        await page.click('text=Sign out');
 
         //claimant see response of respond
         await citizenHubPage.processCitizenHubLogin(params.TestEnvETClaimantEmailAddress, params.TestEnvETClaimantPassword);
-        await citizenHubPage.clicksViewLinkOnClaimantApplicationPage(submissionRef);
+        await citizenHubPage.clicksViewLinkOnClaimantApplicationPage(subRef);
+        await citizenHubPage.validateResponseOfResponse();
+
+    });
+
+
+    test.skip('Legal representatives make and application, caseworker record a decision, LR and citizen view a decision - E/W', async ({ page,citizenHubPage,loginPage,legalRepPage,et1CaseServingPage,caseListPage, applicationTabPage  }) => {
+        const { firstName, lastName } = await et1CaseServingPage.getClaimantFirstName();
+        //perform NOC
+        await page.click('text=Sign out');
+        await page.goto(params.TestUrlForManageCaseAAT);
+        await loginPage.processLogin(params.TestEnvETLegalRepUser, params.TestEnvETLegalRepPassword);
+        await legalRepPage.processNOC('Eng/Wales - Singles', subRef, respondentName, firstName, lastName);
+
+        //legal rep make an application
+        await legalRepPage.legalRepMakeAnApplication();
+        await page.click('text=Sign out');
+
+       //caseworker records a decision
+        await page.goto(params.TestUrlForManageCaseAAT);
+        await loginPage.processLogin(params.TestEnvETCaseWorkerUser, params.TestEnvETPassword);
+        await caseListPage.searchCaseApplicationWithSubmissionReference('Eng/Wales - Singles', subRef);
+        caseNumber = await caseListPage.processCaseFromCaseList();
+        //await applicationTabPage.recordDecision();
+        //Legal rep view decision in an application tab
+        //citizen view notification about decision
 
     });
 
