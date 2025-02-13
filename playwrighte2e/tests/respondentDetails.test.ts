dd98295d31935b38fbc963b715e5dcf1418571b9
gokul-sol <gokul.sridharan@HMCTS.NET>
2025-02-13 16:32:55 +0000

    tests for respondent details

---
+++ playwrighte2e/tests/respondentDetails.test.ts
@@ -0,0 +1,32 @@
+import { test } from '../fixtures/common.fixture';
+import { params } from "../utils/config";
+
+let subRef: string;
+let caseNumber;
+
+test.describe('Respondent details test', () => {
+
+    test.beforeEach(async ({ page, createCaseStep }) => {
+        
+        ({subRef, caseNumber} = await createCaseStep.setupCaseCreatedViaApi(page, "England", "ET_EnglandWales"));
+    });
+
+    test('Englad - Respondent details', async ({ loginPage, caseListPage, respondentDetailsPage, icUploadDocPage }) => {
+
+        await caseListPage.selectNextEvent('Respondent Details');
+        // Check case file view
+        await respondentDetailsPage.processPanelPreference();
+        await caseListPage.navigateToTab('Respondent');
+        await respondentDetailsPage.verifyRespondentDetails();
+
+        //sign out as caseworker
+        await caseListPage.signoutButton();
+                
+        //judge log in
+        await loginPage.processLogin(params.TestEnvETJudgeUserEng, params.TestEnvETJudgeUserEngPassword);
+        await caseListPage.searchCaseApplicationWithSubmissionReference('Eng/Wales - Singles', subRef);
+        await caseListPage.processCaseFromCaseList();
+        await caseListPage.selectNextEvent('Initial Consideration');
+        await icUploadDocPage.verifyRespondentHearingPanelValues();
+    });
+});
