af380070ed9e630b6b404076871de7ad89d118a1
Nilamkanojiya <nilam_kanojiya@yahoo.com>
2025-02-14 17:30:59 +0000

    legal rep record decision

--- playwrighte2e/tests/LegalRepMakeApplication.test.ts
+++ playwrighte2e/tests/LegalRepMakeApplication.test.ts
@@ -2,15 +2,14 @@ import {params} from "../utils/config";
 import { test } from '../fixtures/common.fixture';
 
 const respondentName = 'Mrs Test Auto';
-let subRef: string, submissionRef: string;
+let subRef: string;
 let caseNumber;
 
 test.describe('Make an application and view Recorded Decision', () => {
 
     test.beforeEach(async ({ page,createCaseStep }) => {
-
        ({subRef, caseNumber} = await createCaseStep.setupCUICaseCreatedViaApi(page));
-       // subRef = '1739442740238944';
+        //subRef = '1739442740238944';
     });
 
     test('Legal representatives make and application - England', {tag: '@wip-lr'}, async ({ page,citizenHubPage,loginPage,legalRepPage,et1CaseServingPage,caseListPage, applicationTabPage  }) => {
@@ -48,7 +47,7 @@ test.describe('Make an application and view Recorded Decision', () => {
     });
 
 
-    test.skip('Legal representatives make and application, caseworker record a decision, LR and citizen view a decision - E/W', async ({ page,citizenHubPage,loginPage,legalRepPage,et1CaseServingPage,caseListPage, applicationTabPage  }) => {
+    test('Legal representatives make and application, caseworker record a decision, LR and citizen view a decision - E/W', {tag: '@wip-lr'}, async ({ page,citizenHubPage,loginPage,legalRepPage,et1CaseServingPage,caseListPage, applicationTabPage  }) => {
         const { firstName, lastName } = await et1CaseServingPage.getClaimantFirstName();
         //perform NOC
         await page.click('text=Sign out');
@@ -64,11 +63,18 @@ test.describe('Make an application and view Recorded Decision', () => {
         await page.goto(params.TestUrlForManageCaseAAT);
         await loginPage.processLogin(params.TestEnvETCaseWorkerUser, params.TestEnvETPassword);
         await caseListPage.searchCaseApplicationWithSubmissionReference('Eng/Wales - Singles', subRef);
-        caseNumber = await caseListPage.processCaseFromCaseList();
-        //await applicationTabPage.recordDecision();
-        //Legal rep view decision in an application tab
-        //citizen view notification about decision
+        await caseListPage.processCaseFromCaseList();
+        // await applicationTabPage.recordADecision();
 
+        //Legal rep view decision in an application tab
+        // await applicationTabPage.validateRecordDecisionDetails();
+        // await page.click('text=Sign out');
+
+        //citizen view notification about decision
+        // await citizenHubPage.processCitizenHubLogin(params.TestEnvETClaimantEmailAddress, params.TestEnvETClaimantPassword);
+        // await citizenHubPage.clicksViewLinkOnClaimantApplicationPage(subRef);
+        // await citizenHubPage.validateRecordDecisionBanner();
+        // await page.click('text=Sign out');
     });
 
 });
