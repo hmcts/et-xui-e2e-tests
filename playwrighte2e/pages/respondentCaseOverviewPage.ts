5cf5c37eb1771b41129ee65aabb1321e2681fe9b
Nilamkanojiya <nilam_kanojiya@yahoo.com>
2025-04-04 11:54:50 +0100

    Fix CVE issue  (#468)
    * update dependency
    
    * add Et3 apps test
    
    * refactor accessibility tests
    
    * fix test
    
    * fix failing test
    
    * add delay to legal rep tests
    
    * add respondent app test
    
    * add skip tag
    
    * fix dependency

--- playwrighte2e/pages/respondentCaseOverviewPage.ts
+++ playwrighte2e/pages/respondentCaseOverviewPage.ts
@@ -23,7 +23,49 @@ export default class RespondentCaseOverviewPage extends BasePage {
         await this.page.getByRole('link', {name: 'Respond to the claim'}).waitFor();
         await this.webActions.clickElementByRole('link', {name: 'Respond to the claim'});
         await this.clickStartNow();
+    }
 
+    async respondentMakeApplicationTypeA() {
+        await this.webActions.verifyElementContainsText(this.page.locator('#main-content'), 'Case overview');
+        await this.webActions.verifyElementContainsText(this.page.locator('h3'), 'The tribunal has acknowledged a claim against');
+
+        await this.webActions.clickElementByRole('link', {name: 'Contact the tribunal about my case'});
+        await expect(this.page.locator('#contact-options')).toContainText('Show all sections');
+        await this.webActions.clickElementByRole('button', { name: 'Apply to amend my response ,' });
+        await this.webActions.clickElementByRole('link', { name: 'Apply to amend my response' });
+        await this.page.locator('#contactApplicationText').isVisible();
+
+        // await this.page.getByRole('textbox', { name: 'Document' }).setInputFiles('empty.pdf');
+
+        await this.page.setInputFiles('#contactApplicationFile',`test/data/test.txt`);
+        await this.webActions.clickElementByRole('button', { name: 'Upload file' });
+        await expect(this.page.locator('#contactApplicationFile-hint')).toContainText('You have previously uploaded: test.txt');
+
+        // await page.getByRole('button', { name: 'Upload file' }).click();
+        await this.webActions.fillField('#contactApplicationText', 'Type A application');
+        await this.clickContinue();
+
+        await this.page.locator('#copyToOtherPartyYesOrNo').isVisible();
+        await this.webActions.checkElementByLabel('Yes, I confirm I want to copy');
+        await this.clickContinue();
+
+
+        await expect(this.page.getByRole('heading')).toContainText('Check your answers');
+        await this.submitButton();
+
+        await expect(this.page.locator('h1')).toContainText('You have sent your application to the tribunal');
+        await this.closeAndReturn();
+        await expect(this.page.locator('#main-content')).toContainText('Your request and applications');
+    }
+
+    async validateApplicationTypeA(){
+        await this.webActions.clickElementByRole('link', { name: 'Your request and applications' });
+        await expect(this.page.getByRole('caption')).toContainText('Your applications to the tribunal');
+        await expect(this.page.locator('tbody')).toContainText('In progress');
+
+
+        await this.webActions.clickElementByRole('link', { name: 'Amend my response' });
+        await expect(this.page.locator('dl')).toContainText('Respondent');
     }
 
 }
