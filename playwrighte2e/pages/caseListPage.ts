1f137f4d248988e3d3e68fdbaa5c80d6ffad0fe6
Nilamkanojiya <nilam_kanojiya@yahoo.com>
2025-05-02 14:03:25 +0100

    fix flaky log in

--- playwrighte2e/pages/caseListPage.ts
+++ playwrighte2e/pages/caseListPage.ts
@@ -35,7 +35,8 @@ export default class CaseListPage extends BasePage{
   };
 
     async searchCaseApplicationWithSubmissionReference(option, submissionReference) {
-      await this.page.reload();
+      //await this.page.reload();
+      await this.delay(10000);
       await this.webActions.verifyElementToBeVisible(this.page.locator(this.elements.caseListLink));
 
       await this.webActions.clickElementByCss(this.elements.caseListLink);
