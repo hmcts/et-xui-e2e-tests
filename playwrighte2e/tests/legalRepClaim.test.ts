4d299710d1103167992c180589d485fe8f1fb055
nilam.kanojiya <nilam_kanojiya@yahoo.com>
2025-07-21 17:45:28 +0100

    remove invalid test

--- playwrighte2e/tests/legalRepClaim.test.ts
+++ playwrighte2e/tests/legalRepClaim.test.ts
@@ -3,48 +3,16 @@ import {params} from '../utils/config';
 let caseNumber: any;
 let subRef;
 
+
+test.beforeEach(async ({ page, createCaseStep}) => {
+  ({subRef, caseNumber}  = await createCaseStep.setUpLegalRepCase(page));
+});
+
 test.describe('Legal Representative submits a case and perform various events', () => {
-        test('Claimant Representative creates a claim (England and Wales - Singles) and submit', {tag: '@demo'}, async () => {
-            // let loginPage = new LoginPage(page);
-            // let caseListPage = new CaseListPage(page);
-            // let et1CreateDraftClaim = new Et1CreateDraftClaim(page);
-            // //let applicationPage = new ApplicationPage(page);
-
-
-            // await page.goto(params.TestUrlForManageCaseAAT);
-            // await loginPage.processLogin(params.TestEnvETLegalRepUser, params.TestEnvETLegalRepPassword);
-            // await caseListPage.claimantRepCreateCase('Employment', 'Eng/Wales - Singles', postcode);
-
-            // await et1CreateDraftClaim.et1Section1(claimantsFirstName, claimantsLastName);
-            // await et1CreateDraftClaim.et1Section2(respondentsFirstName, respondentsLastName);
-            // await et1CreateDraftClaim.et1Section3();
-            // let submissionReference = await et1CreateDraftClaim.et1SubmitClaim();
-            // console.log('The value of the Case Number ' + submissionReference);
-
-            //Legal rep makes an application- R92 Yes (offline scenario -only claimant rep is online user)
-            //RET-5191&92
-            // await applicationPage.clickApplicationTab();
-            // await applicationPage.navigateMakeAnApplicationLink();
-            // await applicationPage.makeAnApplicationR92WithYesOption('Yes');
-            //
-            // //Legal rep makes an application- R92 No
-            // await applicationPage.clickApplicationTab();
-            // await applicationPage.navigateMakeAnApplicationLink();
-            // await applicationPage.makeAnApplicationR92WithYesOption('No');
-
-            //TODO NOC-5188, 5190...
-
-            // await page.goto(params.TestUrlForManageCaseAAT);
-            // await loginPage.processLoginOnXui(params.TestEnvETLegalRepUserDiffOrg, params.TestEnvETLegalRepPasswordDiffOrg);
-
-        });
-
 
     test('CR creates a claim, amend claimant/respondent names and persist NOC with original claimant/respondent names',
         {tag: '@demo'},
-        async ({ page, createCaseStep, caseListPage, claimantDetailsPage, respondentDetailsPage, loginPage, nocPage }) => {
-
-        ({subRef, caseNumber}  = await createCaseStep.setUpLegalRepCase(page));
+        async ({ page, caseListPage, claimantDetailsPage, respondentDetailsPage, loginPage, nocPage }) => {
 
         // Amend Claimant and Respondent names
         await caseListPage.selectNextEvent('Claimant Details');
