22922ab8726e36a0de25c60a1dbf5fdcfe3f66ec
nilam.kanojiya <nilam_kanojiya@yahoo.com>
2025-08-18 21:39:37 +0100

    fix CI tests

--- playwrighte2e/tests/ET3RespAndClaimantContactDetails.test.ts
+++ playwrighte2e/tests/ET3RespAndClaimantContactDetails.test.ts
@@ -2,31 +2,9 @@ import { test } from '../fixtures/common.fixture';
 import { params } from '../utils/config';
 
 
-// let caseId: { toString: () => any; };
 let caseNumber: any;
 let subRef;
-const respName ='Mrs Test Auto';
-const firstName ='Grayson';
-const lastName = 'Becker';
-const region = 'England';
 
-
-test.describe('ET3/Respondent Journey', () => {
-  test.beforeEach(async ({ page, createCaseStep }) => {
-    ({ subRef, caseNumber } = await createCaseStep.setupCUICaseCreatedViaApi(page, true, true));
-  });
-
-  test('Respondent Assign a claim (ET3)', { tag: '@demo' }, async ({ et3LoginPage, responseLandingPage, respContactDetailsPages, respClaimantDetails, respContestClaim, respSubmitEt3}) => {
-    //Assign a claim to respondent
-    await et3LoginPage.processRespondentLogin(params.TestEnvET3RespondentEmailAddress, params.TestEnvET3RespondentPassword, caseNumber);
-    await et3LoginPage.replyToNewClaim(subRef, caseNumber, respName, firstName, lastName);
-    await responseLandingPage.startEt3();
-    await respContactDetailsPages.et3Section1();
-    await respClaimantDetails.et3Section2();
-    await respContestClaim.et3Section3();
-    await respSubmitEt3.checkYourAnswers();
-  });
-});
 
 test.describe('ET3/Respondent Journey validates respondent/claimant details', () => {
   test.beforeEach(async ({ page, createCaseStep }) => {
