b8c77997765722f914b231668328694ebfd32c3a
nilam.kanojiya <nilam_kanojiya@yahoo.com>
2025-08-22 09:22:31 +0100

    add RET-3852

--- playwrighte2e/pages/applicationTabPage.ts
+++ playwrighte2e/pages/applicationTabPage.ts
@@ -144,7 +144,7 @@ export class ApplicationTabPage  extends BasePage {
       await this.page.waitForSelector('#tseResponseText', { timeout: 30000 });
       await this.page.fill('#tseResponseText', 'Response of an application');
       await this.webActions.checkElementById('#tseResponseHasSupportingMaterial_Yes');
-      await this.clickContinue();
+      //RET-3852
 
       await this.addNewBtn.click();
       await this.page.waitForSelector('#tseResponseSupportingMaterial_0_uploadedDocument');
