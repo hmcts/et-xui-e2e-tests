5198594c4239a8d9c743ce3d9aa758cd4500aee8
Alwin-Joseph-Christopher <alwinjoseph.christopher@justice.gov.uk>
2026-02-26 11:02:23 +0000

    cleared unused imports

--- playwrighte2e/fixtures/step.fixture.ts
+++ playwrighte2e/fixtures/step.fixture.ts
@@ -1,20 +1,16 @@
 import { test as base } from '@playwright/test';
-import CreateCaseStep from '../steps/createAndAcceptCase';
 import ReferralSteps from '../steps/referralSteps';
 import ET3ProcessingSteps from '../steps/et3ProcessingSteps';
 import AccessibilitySteps from '../steps/accessibilitySteps';
 
 export type StepFixtures = {
-  createCaseStep: CreateCaseStep;
   referralSteps: ReferralSteps;
   et3ProcessingSteps: ET3ProcessingSteps;
   accessibilitySteps: AccessibilitySteps;
 };
 
 export const stepFixtures = base.extend<StepFixtures>({
-  createCaseStep: async ({ page }, use) => {
-    await use(new CreateCaseStep(page));
-  },
+
   referralSteps: async ({ page }, use) => {
     await use(new ReferralSteps(page));
   },
