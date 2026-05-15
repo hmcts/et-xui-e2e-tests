e2e95c78c960e42ba6b34f9c12438fa8e572fc1c
nilam.kanojiya <nilam_kanojiya@yahoo.com>
2026-05-15 14:17:51 +0100

    add RET-6511

---
+++ playwrighte2e/pages/manageTelephoneNotePage.ts
@@ -0,0 +1,26 @@
+import { BasePage } from "./basePage";
+import { expect, Locator, Page } from "@playwright/test";
+
+export default class ManageTelephoneNotePage extends BasePage {
+  private readonly titleField: Locator;
+  private readonly noteField: Locator;
+  private readonly editButton: Locator;
+  private readonly deleteButton: Locator
+  private readonly caseNoteList: Locator
+
+  constructor(page: Page) {
+    super(page);
+    this.editButton = page.locator('#editOrDeleteCaseNote-Edit');
+    this.deleteButton = page.locator('#editOrDeleteCaseNote-Delete');
+    this.titleField = page.locator('#addCaseNote_title');
+    this.noteField = page.locator('#addCaseNote_note');
+      this.caseNoteList = page.locator('#caseNotesList');
+  }
+
+  async editTelephoneNotes() {
+    await this.editButton.check();
+    await this.caseNoteList.selectOption({ index: 1 });
+    await this.noteField.fill('This is amended test');
+    await this.clickSubmitButton();
+  }
+}
