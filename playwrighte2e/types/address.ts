a8c3e77f1a4b3e6d5a0bf523de4ebad7fea7607d
Alwin-Joseph-Christopher <alwinjoseph.christopher@justice.gov.uk>
2026-05-29 14:24:32 +0100

    RET-6237 LR for both claimant and respondent updates contact details (#695)
    * LR update contact details for claimant test
    
    * Amend test for Respondent LR
    
    * Added extra validation in CW XUI

---
+++ playwrighte2e/types/address.ts
@@ -0,0 +1,9 @@
+export type AddressDetails = {
+  addressLine1: string;
+  addressLine2?: string;
+  addressLine3?: string;
+  townOrCity: string;
+  county?: string;
+  postcode: string;
+  country?: string;
+};
